41ab27f4cd37e854a40ecff27b8729d1876afc2c
jahanzaib ikram <ikramjahanzaib@gmail.com>
2022-10-05 11:09:45 +0500

    Updates

---
+++ precache-manifest.4a20b89b3e5d67be6b61521b62ddaa03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eefde3c307d4ee395320",
+    "url": "/my-app/static/css/main.01606bc4.chunk.css"
+  },
+  {
+    "revision": "eefde3c307d4ee395320",
+    "url": "/my-app/static/js/main.eefde3c3.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/my-app/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "ec12d0ec1536c5610702",
+    "url": "/my-app/static/js/2.ec12d0ec.chunk.js"
+  },
+  {
+    "revision": "3af5a2cea1c888c2f435",
+    "url": "/my-app/static/js/runtime~main.3af5a2ce.js"
+  },
+  {
+    "revision": "b4bfd4e0585af4860a9bd2bdbba79df1",
+    "url": "/my-app/index.html"
+  }
+];
